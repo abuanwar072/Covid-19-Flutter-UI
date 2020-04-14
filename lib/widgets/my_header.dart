207a2465d022ec7f4cf505039a92d344e94ebb6e
Jason Kuanca <jason@oval.id>
2020-04-14 11:21:29 +0700

    parallax home ui

--- lib/widgets/my_header.dart
+++ lib/widgets/my_header.dart
@@ -3,23 +3,29 @@ import 'package:covid_19/info_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class MyHeader extends StatelessWidget {
+class MyHeader extends StatefulWidget {
   final String image;
   final String textTop;
   final String textBottom;
-  const MyHeader({
-    Key key,
-    this.image,
-    this.textTop,
-    this.textBottom,
-  }) : super(key: key);
+  final double offset;
+  const MyHeader(
+      {Key key, this.image, this.textTop, this.textBottom, this.offset})
+      : super(key: key);
 
+  @override
+  _MyHeaderState createState() => _MyHeaderState();
+}
+
+class _MyHeaderState extends State<MyHeader> {
   @override
   Widget build(BuildContext context) {
     return ClipPath(
       clipper: MyClipper(),
       child: Container(
-        padding: EdgeInsets.only(left: 40, top: 50, right: 20),
+        padding: EdgeInsets.only(
+            left: 40,
+            top: (50 + widget.offset < 0) ? 0 : 50 + widget.offset,
+            right: 20),
         height: 350,
         width: double.infinity,
         decoration: BoxDecoration(
@@ -59,7 +65,7 @@ class MyHeader extends StatelessWidget {
               child: Stack(
                 children: <Widget>[
                   SvgPicture.asset(
-                    image,
+                    widget.image,
                     width: 230,
                     fit: BoxFit.fitWidth,
                     alignment: Alignment.topCenter,
@@ -68,7 +74,7 @@ class MyHeader extends StatelessWidget {
                     top: 20,
                     left: 150,
                     child: Text(
-                      "$textTop \n$textBottom",
+                      "${widget.textTop} \n${widget.textBottom}",
                       style: kHeadingTextStyle.copyWith(
                         color: Colors.white,
                       ),
